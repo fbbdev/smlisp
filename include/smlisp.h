f618e4f6aa5d223de0b52f3aca2d960a3c09483c
Fabio Massaioli <fabio.massaioli@gmail.com>
2018-09-17 15:06:01 +0200

    Add smlisp header

---
+++ include/smlisp.h
@@ -0,0 +1,13 @@
+#pragma once
+
+#include "builtins.h"
+#include "context.h"
+#include "eval.h"
+#include "hash.h"
+#include "heap.h"
+#include "number.h"
+#include "rbtree.h"
+#include "stack.h"
+#include "util.h"
+#include "value.h"
+#include "word.h"
