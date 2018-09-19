c4a249e4bab782b514f183cc009a739bb276fe66
Fabio Massaioli <fabio.massaioli@gmail.com>
2018-09-19 07:22:14 +0200

    Move lambda form validation to eval module

--- include/eval.h
+++ include/eval.h
@@ -5,3 +5,6 @@
 #include "value.h"
 
 SmError sm_eval(SmContext* ctx, SmValue form, SmValue* ret);
+
+SmError sm_validate_lambda(SmContext* ctx, SmCons* params);
+SmError sm_invoke_lambda(SmContext* ctx, SmCons* lambda, SmCons* params, SmValue* ret);
--- src/builtins.c
+++ src/builtins.c
@@ -1,4 +1,5 @@
 #include "builtins.h"
+#include "eval.h"
 
 void sm_register_builtins(SmContext* ctx) {
     #define REGISTER_BUILTIN_OP(symbol, id) \
@@ -15,42 +16,28 @@ void sm_register_builtins(SmContext* ctx) {
 #define sm_return(value) { *ret = (value); return sm_ok; }
 
 SmError SM_BUILTIN_SYMBOL(print)(SmContext* ctx, SmCons* params, SmValue* ret) {
-    sm_unused(ctx); sm_unused(params); sm_unused(ret);
+    sm_unused(ctx); sm_unused(params);
     sm_return(sm_value_nil());
 }
 
 
 SmError SM_BUILTIN_SYMBOL(cons)(SmContext* ctx, SmCons* params, SmValue* ret) {
-    sm_unused(ctx); sm_unused(params); sm_unused(ret);
+    sm_unused(ctx); sm_unused(params);
     sm_return(sm_value_nil());
 }
 
 SmError SM_BUILTIN_SYMBOL(lambda)(SmContext* ctx, SmCons* params, SmValue* ret) {
-    if (!params) {
-        sm_throw(ctx, SmErrorMissingArguments, "lambda requires at least 1 parameter", false);
-    } else if ((!sm_value_is_list(params->car) && !sm_value_is_word(params->car)) || sm_value_is_quoted(params->car)) {
-        sm_throw(ctx, SmErrorInvalidArgument, "lambda requires an unquoted argument list or word as first parameter", false);
-    } else if (!sm_value_is_list(params->cdr) || sm_value_is_quoted(params->cdr)) {
-        sm_throw(ctx, SmErrorInvalidArgument, "lambda code is a dotted list", false);
-    } else {
-        for (SmCons* arg = params->car.data.cons; arg; arg = sm_list_next(arg)) {
-            if (!sm_value_is_word(arg->car) || sm_value_is_quoted(arg->car))
-                sm_throw(ctx, SmErrorInvalidArgument, "lambda argument lists may only contain unquoted words", false);
-            if ((!sm_value_is_list(arg->cdr) && !sm_value_is_word(arg->cdr)) || sm_value_is_quoted(arg->cdr))
-                sm_throw(ctx, SmErrorInvalidArgument, "lambda argument lists may only contain unquoted words", false);
-        }
+    SmError err = sm_validate_lambda(ctx, params);
+    if (err.code != SmErrorOk)
+        return err;
 
-        for (SmCons* code = params->cdr.data.cons; code; code = sm_list_next(code)) {
-            if (!sm_value_is_list(code->cdr) || sm_value_is_quoted(code->cdr))
-                sm_throw(ctx, SmErrorInvalidArgument, "lambda code is a dotted list", false);
-        }
-    }
+    SmCons* lambda = sm_heap_alloc(&ctx->heap, ctx->frame);
+    *ret = sm_value_cons(lambda);
 
-    SmCons* lambda = sm_heap_alloc_owned(&ctx->heap, ctx->frame);
     lambda->car = sm_value_word(sm_word(&ctx->words, sm_string_from_cstring("lambda")));
     lambda->cdr = sm_value_cons(params);
 
-    sm_return(sm_value_cons(lambda));
+    return sm_ok;
 }
 
 SmError SM_BUILTIN_SYMBOL(quote)(SmContext* ctx, SmCons* params, SmValue* ret) {
@@ -67,165 +54,165 @@ SmError SM_BUILTIN_SYMBOL(quote)(SmContext* ctx, SmCons* params, SmValue* ret) {
 
 
 SmError SM_BUILTIN_SYMBOL(car)(SmContext* ctx, SmCons* params, SmValue* ret) {
-    sm_unused(ctx); sm_unused(params); sm_unused(ret);
+    sm_unused(ctx); sm_unused(params);
     sm_return(sm_value_nil());
 }
 
 SmError SM_BUILTIN_SYMBOL(cdr)(SmContext* ctx, SmCons* params, SmValue* ret) {
-    sm_unused(ctx); sm_unused(params); sm_unused(ret);
+    sm_unused(ctx); sm_unused(params);
     sm_return(sm_value_nil());
 }
 
 
 SmError SM_BUILTIN_SYMBOL(caar)(SmContext* ctx, SmCons* params, SmValue* ret) {
-    sm_unused(ctx); sm_unused(params); sm_unused(ret);
+    sm_unused(ctx); sm_unused(params);
     sm_return(sm_value_nil());
 }
 
 SmError SM_BUILTIN_SYMBOL(cadr)(SmContext* ctx, SmCons* params, SmValue* ret) {
-    sm_unused(ctx); sm_unused(params); sm_unused(ret);
+    sm_unused(ctx); sm_unused(params);
     sm_return(sm_value_nil());
 }
 
 SmError SM_BUILTIN_SYMBOL(cdar)(SmContext* ctx, SmCons* params, SmValue* ret) {
-    sm_unused(ctx); sm_unused(params); sm_unused(ret);
+    sm_unused(ctx); sm_unused(params);
     sm_return(sm_value_nil());
 }
 
 SmError SM_BUILTIN_SYMBOL(cddr)(SmContext* ctx, SmCons* params, SmValue* ret) {
-    sm_unused(ctx); sm_unused(params); sm_unused(ret);
+    sm_unused(ctx); sm_unused(params);
     sm_return(sm_value_nil());
 }
 
 
 SmError SM_BUILTIN_SYMBOL(caaar)(SmContext* ctx, SmCons* params, SmValue* ret) {
-    sm_unused(ctx); sm_unused(params); sm_unused(ret);
+    sm_unused(ctx); sm_unused(params);
     sm_return(sm_value_nil());
 }
 
 SmError SM_BUILTIN_SYMBOL(caadr)(SmContext* ctx, SmCons* params, SmValue* ret) {
-    sm_unused(ctx); sm_unused(params); sm_unused(ret);
+    sm_unused(ctx); sm_unused(params);
     sm_return(sm_value_nil());
 }
 
 SmError SM_BUILTIN_SYMBOL(cadar)(SmContext* ctx, SmCons* params, SmValue* ret) {
-    sm_unused(ctx); sm_unused(params); sm_unused(ret);
+    sm_unused(ctx); sm_unused(params);
     sm_return(sm_value_nil());
 }
 
 SmError SM_BUILTIN_SYMBOL(cdaar)(SmContext* ctx, SmCons* params, SmValue* ret) {
-    sm_unused(ctx); sm_unused(params); sm_unused(ret);
+    sm_unused(ctx); sm_unused(params);
     sm_return(sm_value_nil());
 }
 
 SmError SM_BUILTIN_SYMBOL(caddr)(SmContext* ctx, SmCons* params, SmValue* ret) {
-    sm_unused(ctx); sm_unused(params); sm_unused(ret);
+    sm_unused(ctx); sm_unused(params);
     sm_return(sm_value_nil());
 }
 
 SmError SM_BUILTIN_SYMBOL(cddar)(SmContext* ctx, SmCons* params, SmValue* ret) {
-    sm_unused(ctx); sm_unused(params); sm_unused(ret);
+    sm_unused(ctx); sm_unused(params);
     sm_return(sm_value_nil());
 }
 
 SmError SM_BUILTIN_SYMBOL(cdadr)(SmContext* ctx, SmCons* params, SmValue* ret) {
-    sm_unused(ctx); sm_unused(params); sm_unused(ret);
+    sm_unused(ctx); sm_unused(params);
     sm_return(sm_value_nil());
 }
 
 SmError SM_BUILTIN_SYMBOL(cdddr)(SmContext* ctx, SmCons* params, SmValue* ret) {
-    sm_unused(ctx); sm_unused(params); sm_unused(ret);
+    sm_unused(ctx); sm_unused(params);
     sm_return(sm_value_nil());
 }
 
 
 SmError SM_BUILTIN_SYMBOL(caaaar)(SmContext* ctx, SmCons* params, SmValue* ret) {
-    sm_unused(ctx); sm_unused(params); sm_unused(ret);
+    sm_unused(ctx); sm_unused(params);
     sm_return(sm_value_nil());
 }
 
 SmError SM_BUILTIN_SYMBOL(caaadr)(SmContext* ctx, SmCons* params, SmValue* ret) {
-    sm_unused(ctx); sm_unused(params); sm_unused(ret);
+    sm_unused(ctx); sm_unused(params);
     sm_return(sm_value_nil());
 }
 
 SmError SM_BUILTIN_SYMBOL(caadar)(SmContext* ctx, SmCons* params, SmValue* ret) {
-    sm_unused(ctx); sm_unused(params); sm_unused(ret);
+    sm_unused(ctx); sm_unused(params);
     sm_return(sm_value_nil());
 }
 
 SmError SM_BUILTIN_SYMBOL(cadaar)(SmContext* ctx, SmCons* params, SmValue* ret) {
-    sm_unused(ctx); sm_unused(params); sm_unused(ret);
+    sm_unused(ctx); sm_unused(params);
     sm_return(sm_value_nil());
 }
 
 SmError SM_BUILTIN_SYMBOL(cdaaar)(SmContext* ctx, SmCons* params, SmValue* ret) {
-    sm_unused(ctx); sm_unused(params); sm_unused(ret);
+    sm_unused(ctx); sm_unused(params);
     sm_return(sm_value_nil());
 }
 
 SmError SM_BUILTIN_SYMBOL(caaddr)(SmContext* ctx, SmCons* params, SmValue* ret) {
-    sm_unused(ctx); sm_unused(params); sm_unused(ret);
+    sm_unused(ctx); sm_unused(params);
     sm_return(sm_value_nil());
 }
 
 SmError SM_BUILTIN_SYMBOL(caddar)(SmContext* ctx, SmCons* params, SmValue* ret) {
-    sm_unused(ctx); sm_unused(params); sm_unused(ret);
+    sm_unused(ctx); sm_unused(params);
     sm_return(sm_value_nil());
 }
 
 SmError SM_BUILTIN_SYMBOL(cddaar)(SmContext* ctx, SmCons* params, SmValue* ret) {
-    sm_unused(ctx); sm_unused(params); sm_unused(ret);
+    sm_unused(ctx); sm_unused(params);
     sm_return(sm_value_nil());
 }
 
 SmError SM_BUILTIN_SYMBOL(cadadr)(SmContext* ctx, SmCons* params, SmValue* ret) {
-    sm_unused(ctx); sm_unused(params); sm_unused(ret);
+    sm_unused(ctx); sm_unused(params);
     sm_return(sm_value_nil());
 }
 
 SmError SM_BUILTIN_SYMBOL(cdadar)(SmContext* ctx, SmCons* params, SmValue* ret) {
-    sm_unused(ctx); sm_unused(params); sm_unused(ret);
+    sm_unused(ctx); sm_unused(params);
     sm_return(sm_value_nil());
 }
 
 SmError SM_BUILTIN_SYMBOL(cdaadr)(SmContext* ctx, SmCons* params, SmValue* ret) {
-    sm_unused(ctx); sm_unused(params); sm_unused(ret);
+    sm_unused(ctx); sm_unused(params);
     sm_return(sm_value_nil());
 }
 
 SmError SM_BUILTIN_SYMBOL(cadddr)(SmContext* ctx, SmCons* params, SmValue* ret) {
-    sm_unused(ctx); sm_unused(params); sm_unused(ret);
+    sm_unused(ctx); sm_unused(params);
     sm_return(sm_value_nil());
 }
 
 SmError SM_BUILTIN_SYMBOL(cdddar)(SmContext* ctx, SmCons* params, SmValue* ret) {
-    sm_unused(ctx); sm_unused(params); sm_unused(ret);
+    sm_unused(ctx); sm_unused(params);
     sm_return(sm_value_nil());
 }
 
 SmError SM_BUILTIN_SYMBOL(cddddr)(SmContext* ctx, SmCons* params, SmValue* ret) {
-    sm_unused(ctx); sm_unused(params); sm_unused(ret);
+    sm_unused(ctx); sm_unused(params);
     sm_return(sm_value_nil());
 }
 
 
 SmError SM_BUILTIN_SYMBOL(add)(SmContext* ctx, SmCons* params, SmValue* ret) {
-    sm_unused(ctx); sm_unused(params); sm_unused(ret);
+    sm_unused(ctx); sm_unused(params);
     sm_return(sm_value_nil());
 }
 
 SmError SM_BUILTIN_SYMBOL(sub)(SmContext* ctx, SmCons* params, SmValue* ret) {
-    sm_unused(ctx); sm_unused(params); sm_unused(ret);
+    sm_unused(ctx); sm_unused(params);
     sm_return(sm_value_nil());
 }
 
 SmError SM_BUILTIN_SYMBOL(mul)(SmContext* ctx, SmCons* params, SmValue* ret) {
-    sm_unused(ctx); sm_unused(params); sm_unused(ret);
+    sm_unused(ctx); sm_unused(params);
     sm_return(sm_value_nil());
 }
 
 SmError SM_BUILTIN_SYMBOL(div)(SmContext* ctx, SmCons* params, SmValue* ret) {
-    sm_unused(ctx); sm_unused(params); sm_unused(ret);
+    sm_unused(ctx); sm_unused(params);
     sm_return(sm_value_nil());
 }
